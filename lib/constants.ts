550b4638c9406e865a24ded21e916b6b22592e6c
Sanjeev Sharma <sanjeevcs44@gmail.com>
2025-04-14 12:31:59 +0530

    header and footer

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'ELTStore'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Every Little Thing'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'An E-commerce store built with Next.js and MongoDB'
